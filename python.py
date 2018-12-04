a53541a97c6df890dfd8ad9e4a8746ac62574234
Tomasz Lipinski <Tomasz.lipinski@mycit.ie>
2018-12-04 10:01:29 +0000

    test5

--- python.py
+++ python.py
@@ -1,6 +1,6 @@
 import datetime
 print("!!!Hello world!!!")
-print("Today is a good day")
+print("Today is a cold day day")
 print("Test")
 now = datetime.datetime.now()
 print ("Current date and time is ")
